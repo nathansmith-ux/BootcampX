8d1a2b1e467108a5f6d14a1825350a3cf0800b73
Nathan <nathanrmsmith@gmail.com>
2023-09-22 13:34:16 -0400

    Add queries 1 assignment for SQL learning

---
+++ 1_queries/queries_1.sql
@@ -0,0 +1,25 @@
+SELECT id, name 
+FROM students
+WHERE cohort_id = 1
+ORDER BY name;
+
+SELECT count(*)
+FROM students
+WHERE cohort_id IN (1,2,3);
+
+SELECT name, id, cohort_id
+FROM students
+WHERE email IS NULL OR phone IS NULL;
+
+SELECT name, id, email, cohort_id
+FROM students
+WHERE email NOT LIKE '@gmail.com%' AND phone IS NULL;
+
+SELECT name, id, cohort_id
+FROM students
+WHERE end_date IS NULL 
+ORDER BY cohort_id;
+
+SELECT name, email, phone
+FROM students
+WHERE end_date IS NOT NULL AND github IS NULL;
